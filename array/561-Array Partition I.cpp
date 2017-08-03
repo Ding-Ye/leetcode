c97fa061a0191c170e679fbef45619fb2f5c834f
yeding1992 <1092780440@qq.com>
2017-08-03 15:22:57 +0800

    561

--- array/561-Array Partition I.cpp
+++ array/561-Array Partition I.cpp
@@ -26,3 +26,16 @@ public:
 		return ret;
 	}
 }
+
+//采用排序
+class Solution {
+public:
+  int arrayPairSum(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    int sum = 0;
+    for (int i = 0; i < nums.size(); sum += nums[i], i += 2);
+    return sum;
+  }
+};
+
+
