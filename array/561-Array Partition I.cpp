cb62dc70a924d6396729771c946c92a6e7a0eeca
yeding1992 <1092780440@qq.com>
2017-08-03 15:07:33 +0800

    561

---
+++ array/561-Array Partition I.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+		vector<int> hashtable(20001,0);
+		
+		for(size_t i = 0; i < nums.size(); i++ ){
+			hashtable[nums[i] + 10000] ++;
+		}
+		
+		int ret = 0;
+		int flag   0;
+		
+		for(size_t i = 0; i < 20001;){
+			if((hanstable[i]>0)&&(flag ==0)){
+				ret = ret + i -10000;
+				flag = 1;
+				hanstable[i] --;
+			}
+			else if((hanstable[i]>0)&&(flag == 1)){
+				hanstable[i] --;
+				flag = 0;
+			}
+			else 
+				i++;
+		}
+		return ret;
+	}
+}
