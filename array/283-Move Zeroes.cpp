7b234c9237a95bb7bb2fc9b1af7b4b34c0c1ddda
yeding1992 <1092780440@qq.com>
2017-08-04 15:19:49 +0800

    283
    增加方法二，空间复杂度只有o(1)

--- array/283-Move Zeroes.cpp
+++ array/283-Move Zeroes.cpp
@@ -1,4 +1,4 @@
-///方法二
+///方法一
 //时间复杂度O(n)
 //空间复杂度O(n)
 class Solution {
@@ -19,3 +19,26 @@ public:
 			nums[i] = 0;
     }
 };
+
+
+//方法二
+//时间复杂度0(n)
+//空间复杂度O(1)
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+		
+		int k = 0; //nums中，[0,k)的元素均为非0元素
+		
+		//遍历到第i个元素后，保证[0,i] 中所有非0元素
+		//都按照顺序排列在[0，k)中
+		for(int i = 0; i <nums.size(); i++)
+			if(nums[i])
+				nums[k++] = nums[i];
+		
+		//将nums剩余的位置放置为0
+		for(int i = k; i < nums.size(); i ++)
+				nums[i] = 0;
+};
+
+
