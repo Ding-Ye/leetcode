e2627778496b5a30db19d6fb1354b417c62d4364
yeding1992 <1092780440@qq.com>
2017-08-04 15:36:55 +0800

    方法三

--- array/283-Move Zeroes.cpp
+++ array/283-Move Zeroes.cpp
@@ -41,4 +41,20 @@ public:
 				nums[i] = 0;
 };
 
-
+//方法三
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+		
+		int k = 0; //nums中，[0,k)的元素均为非0元素
+		
+		//遍历到第i个元素后，保证[0,i] 中所有非0元素
+		//都按照顺序排列在[0，k)中
+		//同时，[k，j]为0
+		for(int i = 0; i <nums.size(); i++)
+			if(nums[i])
+				if(i != k)
+					swap(nums[k++],nums[i]);
+				else
+					k++;
+};
