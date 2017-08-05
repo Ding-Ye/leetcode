85c7ff84372b62590a5fbe2294c34b380b07cf96
yeding1992 <1092780440@qq.com>
2017-08-05 17:29:33 +0800

    希尔排序

---
+++ 资料/bubbleSort.cpp
@@ -0,0 +1,14 @@
+template<typename T>
+void bubbleSort(T arr[]. int n){
+	bool swapped;
+	
+	do{
+		swapped = false;
+		
+		for(int i = 1; i < n; i++)
+			if(arr[i-1] > arr[i]){
+				swap(arr[i-1], arr[i]);
+				swapped = true;
+			}
+	}while(swapped);
+}
