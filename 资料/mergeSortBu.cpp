2742052a0af0bda6ac0a34db498185f2ab0eae45
yeding1992 <1092780440@qq.com>
2017-08-05 23:41:42 +0800

    归并排序

--- 资料/mergeSortBu.cpp
+++ 资料/mergeSortBu.cpp
@@ -35,7 +35,7 @@ void __merge(T arr[], int l, int mid, int r){
 template <typename T>
 void mergeSortBu(T arr[], int n){
 	
-	for( int sz = 1; sz <= n; sz += sz )
+	for( int sz = 1; sz <= n/2; sz += sz )
 		for(int i = 0; i +sz < n; i += sz +sz)
 			//对arr[i,i+sz-1] 和 arr [i +sz,i + 2*sz-1] 进行归并
 			__merge(arr, i, i +sz -1, min(i +sz + sz -1, n-1));
