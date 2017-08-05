9d45719d39fa0881ff88c3f8dbf7502f59a75dd3
yeding1992 <1092780440@qq.com>
2017-08-05 21:47:52 +0800

    归并排序

--- 资料/mergeSortBu.cpp
+++ 资料/mergeSortBu.cpp
@@ -38,7 +38,7 @@ void mergeSortBu(T arr[], int n){
 	for( int sz = 1; sz <= n; sz += sz )
 		for(int i = 0; i +sz < n; i += sz +sz)
 			//对arr[i,i+sz-1] 和 arr [i +sz,i + 2*sz-1] 进行归并
-			——merge(arr, i, i +sz -1, min(i +sz + sz -1, n-1));
+			__merge(arr, i, i +sz -1, min(i +sz + sz -1, n-1));
 }
 
 
