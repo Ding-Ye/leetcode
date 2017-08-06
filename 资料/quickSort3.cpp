b7b883f94d44495edf2bdef057bd4583c29d7038
yeding1992 <1092780440@qq.com>
2017-08-06 21:22:21 +0800

    快速排序

--- 资料/quickSort3.cpp
+++ 资料/quickSort3.cpp
@@ -1,4 +1,3 @@
-
 template<typename T>
 void __quickSort3Ways(T arr[], int l ,int r){
 	
@@ -6,6 +5,30 @@ void __quickSort3Ways(T arr[], int l ,int r){
 		insertionSort(arr, l, r);
 		return;
 	}
+	
+	//partition
+	swap(arr[l], arr(rand()%(r-l+1)+l));
+	T v = arr[l];
+	
+	int lt = l;//arr[l+1,lt] < v
+	int gt = r + 1;//arr[gt,r] < v
+	int i = l + 1; //arr[lt+i,i] == v
+	while(i < gt){
+		if(arr[i] < v){
+			swap(arr[i], arr[lt+1]);
+			lt ++;
+			i ++;
+		}
+		
+		else if(arr[i] > v){
+			swap(arr[i], arr[gt-1]);
+			gt --;
+		}
+		else//arr[i] == v
+				i++;
+	}
+	
+	swap(arr[l], arr[lt]);
 }
 
 
