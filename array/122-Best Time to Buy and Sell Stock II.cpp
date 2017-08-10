271a05058b3c333ee57bff43c394a9718c79c7ad
yeding1992 <1092780440@qq.com>
2017-08-10 21:15:27 +0800

    122

--- array/122-Best Time to Buy and Sell Stock II.cpp
+++ array/122-Best Time to Buy and Sell Stock II.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        
-    }
+		int profit = 0;
+		
+		if(prices.size() == 0)
+            return profit;
+
+		for(int i = 0; i < prices.size() - 1; i ++){
+			if(prices[i+1] > prices[i] ){
+				profit += prices[i+1] - prices[i];
+			}
+		
+		}
+		
+		return profit;
+	}
 };
