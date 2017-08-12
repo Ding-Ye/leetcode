4b9e5e9d11f7145242cc828fc2a13ac95418edf3
yeding1992 <1092780440@qq.com>
2017-08-12 22:29:25 +0800

    219

--- array/219-Contains Duplicate II.cpp
+++ array/219-Contains Duplicate II.cpp
@@ -1,3 +1,4 @@
+//Brute ForceBrute Force
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
@@ -12,4 +13,68 @@ public:
 			 return false;
     }
 };
-};
+
+//方法二：哈希表
+class Solution{
+public:
+	bool containsNearbyDuplicate(vector<int>& nums, int k)
+	{
+		unordered_set<int> s;//
+		
+		if(k <= 0)
+			return false;
+		if(k >= nums.size())
+			k = nums.size() - 1;//思维严谨
+		
+		for(int i = 0; i < nums.size(); i++){
+			if(i > k) s.erase(nums[i - k -1]);//删除元素
+			if(s.find(nums[i]) != s.end())
+				return true;
+			s.insert(nums[i]);
+		}
+		return false;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
