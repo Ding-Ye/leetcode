a6cf4107bcb5aff0a7e5dd74e21034c2f9d26448
yeding1992 <1092780440@qq.com>
2017-08-12 23:29:25 +0800

    268

--- array/268-Missing Number.cpp
+++ array/268-Missing Number.cpp
@@ -5,3 +5,33 @@ public:
         return n * (n+1) / 2 - accumulate(begin(nums), end(nums), 0);
     }
 };
+
+//方法二：采用位运算 异或
+class Solution{
+public:
+	int missingNumber(vector<int>& nums){
+		int result = nums.size();
+		int i = 0;
+		
+		//A ^ B ^ B = A   这里运用这一点，如果num和i相等，那么result不变
+		for(int num:nums){
+			result ^= num;//
+			result ^= i;
+			i++;
+		}
+		
+		return result;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
