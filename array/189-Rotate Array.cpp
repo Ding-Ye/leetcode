46eafb7bc86196447091ff90751aa52574c8333d
Ding-Ye <1092780440@qq.com>
2018-01-08 19:43:11 +0800

    189
    换序问题

--- array/189-Rotate Array.cpp
+++ array/189-Rotate Array.cpp
@@ -4,6 +4,8 @@ public:
     void rotate(vector<int>& nums, int k) {
         int *aux = new int[k+1];
 		int n = nums.size();
+		if(k > n && k < 0)
+			k = 0;
 		for(int i = 0; i < k+1; i ++){
 			aux[i] = nums[i];
 		}
@@ -20,9 +22,36 @@ public:
 };
 
 //方法二:
+class Solution{
+public:
+	void rotate(vector<int>& nums, int k){
+		int n = nums.size();
+		k = k % n; //k对n取余数
+		for(int i = 0; i <=(n - 1 - k)/2; i++){
+			swap(nums[i], nums[n-1-k-i]);
+		}
+		for(int i = n - k; i <= (n-1 + n-k)/2; i++){
+			swap(nums[i], nums[n - 1 + n - k - i]);
+		}
+		for(int i = 0; i <= (n - 1)/2; i++){
+			swap(nums[i], nums[n - 1 - i]);
+		}
+	}
+};
 
-
-
+//方法三：
+class Solution{
+public:
+	void rotate (vector<int>& nums, int k){
+		int n = nums.size();
+		if(k == n | k == 0)
+			return;
+		k %= n; //k = k % n;
+		reverse(nums.begin(), nums.begin() + n - k); //reverse 就是反转
+		reverse(nums.begin() + n - k, nums.end());
+		reverse(nums.begin(), nums.end());
+	}
+};
 
 
 
